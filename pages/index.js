afc888387d71faf221f3b33d847eb1c0d3842d82
Bahtiar <zam@kitalulus.com>
2020-09-19 09:26:38 +0700

    add link to ccroutine

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -18,10 +19,12 @@ export default function Home() {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
+          <Link href="/whats-is-ccroutine">
+          <a className="card">
             <h3>CCRoutine &rarr;</h3>
             <p>What is?</p>
           </a>
+          </Link>
 
           <a href="https://nextjs.org/learn" className="card">
             <h3>CCClass &rarr;</h3>
