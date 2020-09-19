7d3e0df765fb4f764d45a2b3ee3a0a25d9a7e630
Bahtiar <zam@kitalulus.com>
2020-09-19 09:07:47 +0700

    add activities

--- pages/index.js
+++ pages/index.js
@@ -10,39 +10,39 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to Computer Club
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          What is <code>Computer Club?</code>
         </p>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>CCRoutine &rarr;</h3>
+            <p>What is?</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>CCClass &rarr;</h3>
+            <p>What is?</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className="card"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>CCJourney &rarr;</h3>
+            <p>What is?</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>CCDedication &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              What is?
             </p>
           </a>
         </div>
