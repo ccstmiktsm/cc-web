c63407dd794b5da0877f9706b5ba70a0f53b8f03
dedesaepulloh <dedesaepulloh77@gmail.com>
2020-10-10 10:30:18 +0700

    add cc journey

--- pages/index.js
+++ pages/index.js
@@ -37,10 +37,10 @@ export default function Home() {
           </a>
 
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="/journey"
             className="card"
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>CC JOURNEY &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
